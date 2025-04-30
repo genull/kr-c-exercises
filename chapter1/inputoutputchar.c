941c342404838422e69ad879a1d0270931e54568
genull <zestq@proton.me>
2025-04-30 13:57:10 -0300

    add exercises 1.6 and 1.7

---
+++ chapter1/inputoutputchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* copy input to output; 2nd version */
+
+int main() {
+  int c;
+
+  c = getchar();
+  printf("%d\n", c != EOF);
+  //printf("%d\n", getchar() != EOF);
+  // c = getchar();
+  //  while ((c = getchar()) != EOF) {
+    //putchar(c);
+    //c = getchar();
+
+  //}
+
+
+}
