0f9d4669951a8f59d1bfc9c886cfc70580b7679a
genull <zestq@proton.me>
2025-04-29 21:01:30 -0300

    use symbolic constants

--- chapter1/forstatement.c
+++ chapter1/forstatement.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/* lower and upper are reversed because of exercise */
+
+#define LOWER 300      /* lower limit of table */
+#define UPPER 0        /* upper limit */
+#define STEP 20        /* step size */
+
 /* print Fahrenheit-Celsius table */
 int main() {
   int fahr;
 
-  for(fahr = 300; fahr >= 0; fahr = fahr - 20)
+  for(fahr = LOWER; fahr >= UPPER; fahr = fahr - STEP)
     printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
 }
